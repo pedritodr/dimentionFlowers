78da6708669ada8a98a458dbea6dbbca03f99ddf
Pedro Duran <pedroduran014@gmail.com>
2021-08-24 15:31:34 -0500

    pipelines

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+echo "Deploy script started"
+cd /var/www/html/dimention
+git pull origin master
+echo "Deploy script finished execution"
+exit
+#comentario de prueba para notar el deployment
+#segundo comentario para la prueba ok
